675c2d7734ed6d22befd9076a1007c3d616e2e10
mvk-savi <mkeffer@savitechnology.net>
2015-09-10 13:12:14 -0400

    This is just a test pull request.  DO NOT approve
    Just a test

--- scripts/dev-producer.sh
+++ scripts/dev-producer.sh
@@ -5,3 +5,5 @@ mv /opt/src/target/RELEASE/KafkaHttp_2.9.2-0.0.1/ /opt/kafka-http
 chmod +x /opt/kafka-http/bin/*.sh
 mkdir -p /var/log/kafka-http
 BROKER_LIST=localhost:9092 /opt/scripts/start-producer.sh
+
+# this is a test to demonstrate that a read only user can create a pull request.  please reject this request
